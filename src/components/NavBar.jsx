07f4ba28450de00ef9ee8df4ab06a29060102287
vimalpksh <itisvimal@gmail.com>
2024-07-18 18:55:09 +0530

    navbar component completed, more basic assetsadded

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaTwitterSquare />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
